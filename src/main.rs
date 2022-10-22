fc6e5075f10630c94d3cd7f0ebf435a19851dbad
WoMspace <alex0002b@gmail.com>
2022-10-22 15:43:15 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use clap::Parser;
+use stopwatch::{Stopwatch};
+mod numlang;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// The maximum candidate to check for primes.
+    #[arg(short = 'c', long)]
+    max_candidate: Option<u32>,
+
+    /// The number of primes to search for.
+    #[arg(short = 'g', long = "goal", default_value_t = 1_000_000)]
+    prime_goal: u32,
+
+    /// How often to report major statistics. Set to 0 to disable.
+    #[arg(short = 'm', long = "major", default_value_t = 1_000_000)]
+    major_interval: u32,
+
+    /// How often to report minor statistics. Set to 0 to disable.
+    #[arg(short = 'n', long = "minor", default_value_t = 10_000)]
+    minor_interval: u32,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut primes: Vec<u32> = vec![3];
+
+    let loop_end: u32 = args.max_candidate.unwrap_or(u32::MAX);
+
+    let mut major_timer = Stopwatch::start_new();
+    let mut minor_timer = Stopwatch::start_new();
+
+    for candidate in (5..loop_end).step_by(2) {
+        if check_prime(candidate, &primes) {
+            
+            primes.push(candidate);
+            {
+                let len: u32 = (primes.len() + 1) as u32;
+
+                if args.major_interval != 0 && len % args.major_interval == 0 {
+                    major_report(args.major_interval, &mut major_timer, candidate);
+                }
+                else if args.minor_interval != 0 && len % args.minor_interval == 0 {
+                    minor_report(args.minor_interval, &mut minor_timer, primes.len(), &primes);
+                }
+            }
+            if primes.len() as u32 >= args.prime_goal { break; }
+
+        }
+    }
+
+    
+    fn check_prime(candidate: u32, prime_array: &Vec<u32>) -> bool {
+        let sqrt: u32 = (candidate as f32).sqrt() as u32;
+
+        for prime in prime_array {
+            if candidate % prime == 0 {
+                return false;
+            }
+            if prime > &sqrt { break; }
+        }
+        return true;
+    }
+
+    fn minor_report(minor_interval: u32, minor_timer: &mut Stopwatch, last_index: usize, prime_array: &Vec<u32>) {
+        if minor_interval == 0 { return; }
+
+        let nth = (last_index + 1) as u32 / minor_interval;
+        let prime = prime_array[last_index - 1];
+        let time: String;
+        if minor_timer.elapsed_ms() < 5000 {
+            time = format!("{}ms", minor_timer.elapsed_ms());
+        }
+        else {
+            time = format!("{:.2}s", minor_timer.elapsed().as_secs_f32())
+        }
+
+        println!("{} | {} | {}", nth, prime, time);
+        minor_timer.restart();
+    }
+    fn major_report(major_interval: u32, major_timer: &mut Stopwatch, last_prime: u32) {
+        if major_interval == 0 { return; }
+
+        let avg = major_interval as f32 / major_timer.elapsed().as_secs_f32();
+        let timer_text: String = format!("{:.3}", major_timer.elapsed().as_secs_f32());
+        let avg_text: String = format!("{:.3}", avg);
+        let last_text: String = numlang::get_name(major_interval);
+
+        println!("Last {} took {}s", last_text, timer_text);
+        println!("Last {}th prime is {}", last_text, last_prime);
+        println!("Average speed: {} primes/second", avg_text);
+    }
+}
---
+++ src/numlang.rs
@@ -0,0 +1,20 @@
+pub fn get_name(num: u32) -> String {
+    let digits: i32 = format!("{}", num).len() as i32;
+    match digits {
+        3 => "hundred".to_string(),
+        4 => "thousand".to_string(),
+        5 => "ten thousand".to_string(),
+        6 => "hundred thousand".to_string(),
+        7 => "million".to_string(),
+        8 => "ten million".to_string(),
+        9 => "hundred million".to_string(),
+        10 => "billion".to_string(),
+        11 => "ten billion".to_string(),
+        12 => "hundred billion".to_string(),
+        13 => "trillion".to_string(),
+        14 => "ten trillion".to_string(),
+        15 => "hundred trillion".to_string(),
+        16 => "quadrillion".to_string(),
+        _ => "ERR".to_string()
+    }
+}
